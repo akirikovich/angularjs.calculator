fc0bb146068a0032b9d7db290d0c1bf5f377d7e6
Alexandr Kirikovich <mail@akirikovich.ru>
2015-07-29 17:17:49 +0300

    Add directive

--- calculator.js
+++ calculator.js
@@ -1,41 +1,114 @@
 (function() {
 	var app = angular.module("simpleCalculator", []);
 
-	app.controller("calculatorController", ["$scope", function() {
-		var calculator = this;
+	app.directive("calculator", function() {
+		return {
+			restrict: "E",
+			template: function(element, attributes) {
+				var settings = angular.fromJson(attributes.settings),
+					form = [];
 
-		calculator.count = null;
-		calculator.speed = 5;
-		calculator.cms = 0;
-		calculator.responsive = false;
-		calculator.retina = false;
-		calculator.microdata = false;
+				for(var i = 0, len = settings["items"].length; i < len; i++) {
+					var elem = settings["items"][i];
 
-		calculator.calc = function() {
-			calculator.result = calculator.count * 25;
-			calculator.result = calculator.result * (1 + (calculator.speed - 5) / 20);
-			switch(calculator.cms) {
-				case "2":
-					calculator.result += calculator.result * 1.1;
-				break;
-				case "3":
-					calculator.result += calculator.result * 1.15;
-				break;
-				case "4":
-					calculator.result += calculator.result * 1.2;
-				break;
-			}
+					form.push('<div class="form-group">');
+					switch(elem.field.type) {
+						case "select":
+							form.push('<select \
+											class="form-control" \
+											ng-change="calculator.reCalculate()" \
+											ng-model="calculator.' + elem.field.model + '" \
+								>');
+							angular.forEach(elem.field.options, function(value, name) {
+								form.push('<option value="' + value + '">' + name + '</option>')
+							});
+							form.push('</select>');
+						break;
+						case "checkbox":
+							form.push('<div class="checkbox"> \
+											<label> \
+											<input \
+												type="' + elem.field.type + '" \
+												ng-change="calculator.reCalculate()" \
+												ng-model="calculator.' + elem.field.model + '"/> \
+											' + elem.label + ' \
+											</label> \
+										</div> \
+							');
+						break;
+						default:
+							if(elem.label) {
+								form.push('<label>' + elem.label + '</label>');
+							}
+							form.push('<input \
+											type="' + elem.field.type + '" \
+											ng-change="calculator.reCalculate()" \
+											ng-model="calculator.' + elem.field.model + '"');
+							if(elem.field.type !== "range") {
+								form.push('class="form-control"');
+							}
+							angular.forEach(elem.field.attrs, function(value, name) {
+								form.push(name + '="' + value + '"');
+							});
+							form.push("/>");
+						break;
+					}
+					form.push('</div>');
 
-			if(calculator.responsive) {
-				calculator.result = calculator.result * 1.2;
-			}
-			if(calculator.retina) {
-				calculator.result += 50;
-			}
-			if(calculator.microdata) {
-				calculator.result += calculator.count * 3;
-			}
+				}
+				form.push('\
+					<div class="form-group">\
+						<div class="alert alert-success">\
+							<b>Result:</b>\
+							<span>{{calculator.result | currency}}</span>\
+						</div>\
+					</div>\
+				');
+
+				return form.join("");
+			},
+			controller: function($scope) {
+				$scope.calculator = {
+					count: 1,
+					speed: 5,
+					cms: 0,
+					responsive: false,
+					retina: false,
+					microdata: false,
+					reCalculate: function() {
+						var calculator = this;
+
+						calculator.result = calculator.count * 25;
+						calculator.result = calculator.result * (1 + (calculator.speed - 5) / 20);
+						switch(calculator.cms) {
+							case "2":
+								calculator.result *= 1.5;
+							break;
+							case "3":
+								calculator.result *= 1.6;
+							break;
+							case "4":
+								calculator.result *= 1.7;
+							break;
+						}
+
+						if(calculator.responsive) {
+							calculator.result = calculator.result * 1.2;
+						}
+						if(calculator.retina) {
+							calculator.result += 50;
+						}
+						if(calculator.microdata) {
+							calculator.result += calculator.count * 3;
+						}
+					}
+				};
+				
+				$scope.calculator.reCalculate();
+
+			},
+			contollerAs: "calculator"
 		}
+	});
 
-	}]);
 })();
--- readme.md
+++ readme.md
@@ -1,4 +1,47 @@
+## Example
+
+<pre><code>
+						<calculator settings='{
+							"items": [
+								{
+									"field": {
+										"type": "number",
+										"model": "count"
+									}
+								}, {
+									"label": "Work speed",
+									"field": {
+										"type": "range",
+										"model": "speed",
+										"attrs": {
+											"min": "1",
+											"max": "10"
+										}
+									}
+								}, {
+									"field": {
+										"type": "select",
+										"model": "cms",
+										"options": {
+											"Without CMS": "1",
+											"NetCat": "2",
+											"1C Bitrix": "3",
+											"Wordpress": "4"
+										}
+									}
+								}, {
+									"label": "Responsive",
+									"field": {
+										"type": "checkbox",
+										"model": "responsive"
+									}
+								}
+							]
+						}'>
+						</calculator>
+</code></pre>
+
 ## Roadmap
 
-+ Add calculator into custom element (Angular directory)
-+ Replace parameters into the attribute 
++ Replace recalculation function from the js file
++ Minimize settings...
